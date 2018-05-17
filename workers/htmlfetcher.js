78163762e62d71aca3d653e1921c6138fda29b03
aliu730 <aliu730@gmail.com>
2018-05-17 12:31:27 -0700

    dls sites

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,11 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+
+exports.run = function() {
+  archive.readListOfUrls(function (urlsArray) {
+    archive.downloadUrls(urlsArray);
+  });
+};
+
+this.run();
